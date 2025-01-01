7174172fe5f6868faedc264e7c15618b2eac24a8
Leialoha <github@leialoha.dev>
2025-01-01 00:11:10 -0600

    Fixed error handling for API Keys

--- src/routes/middleware/apiKey.ts
+++ src/routes/middleware/apiKey.ts
@@ -3,11 +3,19 @@ import { NextFunction, Request, Response } from "express";
 import * as AuthenticationAspect from "../../aspects/AuthenticationAspect";
 
 export const validateApiKey = (req: Request, res: Response, next: NextFunction) => {
-    AuthenticationAspect.validateHeaders(req);
-    next();
+    try {
+        AuthenticationAspect.validateHeaders(req);
+        next();
+    } catch (e) {
+        next(e);
+    }
 }
 
 export const validateMasterApiKey = (req: Request, res: Response, next: NextFunction) => {
-    AuthenticationAspect.validateHeadersMaster(req);
-    next();
+    try {
+        AuthenticationAspect.validateHeadersMaster(req);
+        next();
+    } catch (e) {
+        next(e);
+    }
 }
